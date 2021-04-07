194fa9731442efd1fda40e50f6019d19c2b2487b
Dmitrii Nasedkin <2_tone@mail.ru>
2021-04-07 19:02:28 +0300

    code done

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[]args){
+        long TicketPrice = 1500;
+        int Bonus = 20;
+
+        long TotalBonus = TicketPrice / Bonus;
+        System.out.println(TotalBonus);
+    }
+}
